64b8d958a6752e21e3864c6e06a4d0c94f3c71e1
Nathan Cochran <NathanJCochran@gmail.com>
2015-12-23 22:04:11 -0500

    Booyah

--- main.go
+++ main.go
@@ -1,14 +1,25 @@
 package fallfishunlimited
 
 import (
-	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
+var (
+	indexHtml []byte
+)
+
 func init() {
+	content, err := ioutil.ReadFile("index.html")
+	if err != nil {
+		panic(err)
+	}
+
+	indexHtml = content
+
 	http.HandleFunc("/", handler)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Ho. Ly. Shit")
+	w.Write(indexHtml)
 }
